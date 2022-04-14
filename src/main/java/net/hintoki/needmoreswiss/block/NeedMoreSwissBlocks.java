a218aa7d863db635faa937e6a15a5bcf7a63e304
Hintoki <102687288+HintokiTheRat@users.noreply.github.com>
2022-04-14 03:55:08 +0300

    swiss

--- src/main/java/net/hintoki/needmoreswiss/block/NeedMoreSwissBlocks.java
+++ src/main/java/net/hintoki/needmoreswiss/block/NeedMoreSwissBlocks.java
@@ -10,6 +10,7 @@ import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -28,51 +29,51 @@ public class NeedMoreSwissBlocks {
             new LeavesBlock(FabricBlockSettings.copy(Blocks.OAK_LEAVES).nonOpaque()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_LOG = registerBlock("colorata_log",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block STRIPPED_COLORATA_LOG = registerBlock("stripped_colorata_log",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_WOOD = registerBlock("colorata_wood",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block STRIPPED_COLORATA_WOOD = registerBlock("stripped_colorata_wood",
-            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
 
     public static final Block COLORATA_PLANKS = registerBlock("colorata_planks",
-            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
 
 
     public static final Block COLORATA_BUTTON = registerBlock("colorata_button",
-            new NeedMoreSwissWoodenButtonBlock(FabricBlockSettings.copy(Blocks.OAK_BUTTON).strength(2f).requiresTool().noCollision()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new NeedMoreSwissWoodenButtonBlock(FabricBlockSettings.copy(Blocks.OAK_BUTTON).strength(2f).noCollision()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_PRESSURE_PLATE = registerBlock("colorata_pressure_plate",
             new NeedMoreSwissPressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.copy(Blocks.OAK_PRESSURE_PLATE)
-                    .strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+                    .strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_FENCE = registerBlock("colorata_fence",
-            new FenceBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new FenceBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_FENCE_GATE = registerBlock("colorata_fence_gate",
-            new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_LOGS_WALL = registerBlock("colorata_logs_wall",
-            new WallBlock(FabricBlockSettings.of(Material.WOOD).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new WallBlock(FabricBlockSettings.of(Material.WOOD).strength(2f).sounds(BlockSoundGroup.WOOD)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_SLAB = registerBlock("colorata_slab",
-            new SlabBlock(FabricBlockSettings.copy(Blocks.OAK_SLAB).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            new SlabBlock(FabricBlockSettings.copy(Blocks.OAK_SLAB).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_STAIRS = registerBlock("colorata_stairs", new NeedMoreSwissStairsBlock(NeedMoreSwissBlocks.COLORATA_PLANKS
-            .getDefaultState(), FabricBlockSettings.copy(Blocks.OAK_STAIRS).strength(2f).requiresTool()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            .getDefaultState(), FabricBlockSettings.copy(Blocks.OAK_STAIRS).strength(2f)), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
 
     public static final Block COLORATA_DOOR = registerBlock("colorata_door", new NeedMoreSwissDoorBlock(
-            FabricBlockSettings.copy(Blocks.OAK_DOOR).strength(2f).requiresTool().nonOpaque()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            FabricBlockSettings.copy(Blocks.OAK_DOOR).strength(2f).nonOpaque()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
     public static final Block COLORATA_TRAPDOOR= registerBlock("colorata_trapdoor", new NeedMoreSwissTrapdoorBlock(
-            FabricBlockSettings.copy(Blocks.OAK_TRAPDOOR).strength(2f).requiresTool().nonOpaque()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
+            FabricBlockSettings.copy(Blocks.OAK_TRAPDOOR).strength(2f).nonOpaque()), NeedMoreSwissItemGroup.CRIMERZBOOGALOO_GROUP);
 
 
 
